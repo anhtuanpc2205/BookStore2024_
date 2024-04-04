029cc880bcf7761d0631b48f5987d3faba0b7a88
anhtuanpc2205 <147957110+anhtuanpc2205@users.noreply.github.com>
2024-04-04 11:45:23 +0700

    JS in Cart View

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddDistributedMemoryCache();
 
 builder.Services.AddSession(options =>
 {
-    options.IdleTimeout = TimeSpan.FromSeconds(1200);
+    options.IdleTimeout = TimeSpan.FromMinutes(20);
     options.Cookie.HttpOnly = true;
     options.Cookie.IsEssential = true;
 });
