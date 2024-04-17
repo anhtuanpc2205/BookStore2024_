9a53c5819b9f31dbcf098c9ae11b4c197f86fd1a
anhtuanpc2205 <147957110+anhtuanpc2205@users.noreply.github.com>
2024-04-17 21:48:25 +0700

    change some <div> tag in ProductItem partial view, add Best Selling Books component (use in Home Page)

---
+++ ViewComponents/BestSellingBooksViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using BookStore2024.Data;
+using BookStore2024.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace BookStore2024.ViewComponents
+{
+    public class BestSellingBooksViewComponent : ViewComponent
+    {
+        private readonly BookStoreContext DBContext;
+        public BestSellingBooksViewComponent(BookStoreContext DatabaseContext) => DBContext = DatabaseContext;
+        public IViewComponentResult Invoke()
+        {
+           
+            var data = DBContext.ViewBestSelling.Select(p => new ProductVM
+            {
+                BookDetailId = p.BookDetailId,
+                CategoryId = p.CategoryId,
+                CategoryName = p.CategoryName,
+                ProductName = p.BookTitle,
+                ProductImg = p.BookImageUrl ?? "",
+                AuthorName = p.AuthorName,
+                Price = p.Price,
+                Discount = p.Discount,
+                FormatName = p.FormatName
+            });
+            return View(data);
+        }
+    }
+}
