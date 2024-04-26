38d3bda86428c98c2982a555dea67a2b7bc1effa
anhtuan2205 <57052425+anhtuan2205@users.noreply.github.com>
2024-04-26 17:02:39 +0700

    test Mega-Menu

---
+++ ViewComponents/MegaMenuViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using BookStore2024.Data;
+using BookStore2024.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore2024.ViewComponents
+{
+    public class MegaMenuViewComponent : ViewComponent
+    {
+        private readonly BookStoreContext DBContext;
+        public MegaMenuViewComponent(BookStoreContext DatabaseContext) => DBContext = DatabaseContext;
+
+        public IViewComponentResult Invoke()
+        {
+            var data = DBContext.TblCategories.Select(Cate => new CategoriesMenuVM
+            {
+                CategoryId = Cate.CategoryId,
+                CategoryName = Cate.CategoryName
+            });
+
+            return View("Default", data);
+        }
+    }
+}
