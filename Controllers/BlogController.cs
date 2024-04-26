2271fbf3bf4f8ef3edd779021494d572c049b18a
anhtuanpc2205 <147957110+anhtuanpc2205@users.noreply.github.com>
2024-04-26 09:10:59 +0700

    Blog Index

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,48 @@
+﻿using BookStore2024.Data;
+using BookStore2024.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore2024.Controllers
+{
+	public class BlogController : Controller
+	{
+		private readonly BookStoreContext DBContext;
+		public BlogController(BookStoreContext DatabaseContext) => DBContext = DatabaseContext;
+		public IActionResult Index()
+		{
+			var BlogQuery = DBContext.ViewBlogDetail.AsQueryable();
+
+			var data = BlogQuery.OrderByDescending(p => p.Views).Take(9).Select(p => new BlogVM
+			{
+				BlogId = p.BlogId,
+				BlogTitle = p.BlogTitle,
+				BlogDescription = p.BlogDescription,
+				Content = p.BlogContent,
+				AuthorId = p.AuthorId,
+				ImgUrl = p.BlogImageUrl,
+				Views = p.Views,
+				AuthorName = p.AuthorName
+			});
+
+			return View(data);
+		}
+		public IActionResult Detail(int DetailID)
+		{
+			var BlogQuery = DBContext.ViewBlogDetail.AsQueryable().Where(p => p.BlogId == DetailID);
+
+			var data = BlogQuery.Select(p => new BlogVM
+			{
+				BlogId = p.BlogId,
+				BlogTitle = p.BlogTitle,
+				BlogDescription = p.BlogDescription,
+				Content = p.BlogContent,
+				AuthorId = p.AuthorId,
+				ImgUrl = p.BlogImageUrl,
+				Views = p.Views,
+				AuthorName = p.AuthorName
+			}).SingleOrDefault();
+
+			return View(data);
+		}
+	}
+}
--- ScriptSQLServer/CREATE_BookStore_DataBase.sql
+++ ScriptSQLServer/CREATE_BookStore_DataBase.sql
@@ -811,6 +811,9 @@ UPDATE tbl_Book_Detail
 SET views_ = 30
 WHERE price >= 15
 
+UPDATE tbl_Blog SET views_= 30 WHERE blog_id < 5
+UPDATE tbl_Blog SET views_= 12 WHERE blog_id > 5 AND blog_id < 10
+
 UPDATE tbl_Author SET author_description_ = 'Consectetur adipisicing elit sed do eiusmod tempor incididunt labore toloregna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamcoiars nisiuip commodo consequat aute irure dolor in aprehenderit aveli esseati cillum dolor fugiat nulla pariatur cepteur sint occaecat cupidatat.
 Caanon proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnisate natus error sit voluptatem accusantium doloremque totam rem aperiam, eaque ipsa quae abillo inventoe veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia.
 Voluptas sit asapernatur aut odit aut fugit, sed quia consequuntur magni dolores eos quistan ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.'
