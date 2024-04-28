7ec939e84f58ac86e92a138b2fa815908c3fec33
anhtuan2205 <57052425+anhtuan2205@users.noreply.github.com>
2024-04-28 19:28:20 +0700

    test regis page

---
+++ Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using BookStore2024.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore2024.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly BookStoreContext DBContext;
+        public UserController(BookStoreContext DatabaseContext) => DBContext = DatabaseContext;
+
+        public IActionResult Registering()
+        {
+            return View();
+        }
+    }
+}
--- ViewModels/RegisterVM.cs
+++ ViewModels/RegisterVM.cs
@@ -1,19 +1,30 @@
-﻿namespace BookStore2024.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore2024.ViewModels
 {
     public class RegisterVM
     {
-        
+        [Required(ErrorMessage ="*")]
         public int UserId { get; set; }
 
+
+        [Required(ErrorMessage = "Enter username,Email and Password")]
+        [MaxLength(50,ErrorMessage = "maximum is 50 characters.")]
         public string UserName { get; set; }
 
+
+        [Required(ErrorMessage = "Enter username,Email and Password")]
+        [MaxLength(100, ErrorMessage = "maximum is 100 characters.")]
         public string Email { get; set; }
 
-        public string Password { get; set; }
 
+        [Required(ErrorMessage = "Enter username,Email and Password")]
+        [MaxLength(20, ErrorMessage = "maximum is 20 characters.")]
+        public string Password { get; set; }
+        
         public string? ShippingAddress { get; set; }
 
-        public byte Role { get; set; }
+        public byte Role { get; set; } = 2;
 
         public string? ProfileImageUrl { get; set; }
     }
