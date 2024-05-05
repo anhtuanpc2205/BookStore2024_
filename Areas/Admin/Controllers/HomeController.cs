deaaf0956c9351e8892c364e107f0b87c5ccd0d6
anhtuan2205 <57052425+anhtuan2205@users.noreply.github.com>
2024-05-05 10:28:23 +0700

    Index UI

--- Areas/Admin/Controllers/HomeController.cs
+++ Areas/Admin/Controllers/HomeController.cs
@@ -1,13 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookStore2024.Data;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace BookStore2024.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class HomeController : Controller
     {
-        [Area("Admin")]
+        private readonly BookStoreContext db;
+        public HomeController(BookStoreContext DatabaseContext) => db = DatabaseContext;
         public IActionResult Index()
         {
+            var chartData = new[]
+            {
+                new { value = 1048, name = "Search Engine" },
+                new { value = 735, name = "Direct" },
+                new { value = 580, name = "Email" },
+                new { value = 484, name = "Union Ads" },
+                new { value = 700, name = "Video Ads" }
+            };
+
+            // Chuyển đổi dữ liệu thành chuỗi JSON
+            string jsonData = JsonConvert.SerializeObject(chartData);
+
+            // Truyền dữ liệu JSON vào view
+            ViewBag.ChartData = jsonData;
+
             return View();
+        }
+        public IActionResult ProductList() { 
+            var products_ = db.TblBooks.ToList();
+            return View(products_); 
         }
     }
 }
