143ff6461fe342aeaa52789a4c9783eaf2461af0
anhtuan2205 <57052425+anhtuan2205@users.noreply.github.com>
2024-05-06 19:22:21 +0700

    Delete - Create products complete

--- Areas/Admin/Controllers/AdminProductsController.cs
+++ Areas/Admin/Controllers/AdminProductsController.cs
@@ -4,6 +4,7 @@ using BookStore2024.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.SqlServer.Server;
 using System.Net;
 
 namespace BookStore2024.Areas.Admin.Controllers
@@ -13,6 +14,8 @@ namespace BookStore2024.Areas.Admin.Controllers
     {
         private readonly BookStoreContext DBContext;
         public AdminProductsController(BookStoreContext DatabaseContext) => DBContext = DatabaseContext;
+
+        #region Display
         public IActionResult ProductList(string? searchString)
         {
             var ProductsQuery = DBContext.ViewBookDetails.AsQueryable();
@@ -32,6 +35,9 @@ namespace BookStore2024.Areas.Admin.Controllers
 
             return View(data);
         }
+        #endregion
+
+        #region Create
         [HttpGet]
         public IActionResult Create()
         {
@@ -47,13 +53,25 @@ namespace BookStore2024.Areas.Admin.Controllers
                 Value = p.GenreId.ToString()
             }).ToList();
 
+            var formatList = DBContext.TblFormats.Select(p => new SelectListItem()
+            {
+                Text = p.FormatName,
+                Value = p.FormatId.ToString()
+            }).ToList();
+
             cateList.Insert(0, new SelectListItem()
             {
                 Text = "---select---",
                 Value = "0"
             });
 
-            genreList.Insert(0, new SelectListItem
+            genreList.Insert(0, new SelectListItem()
+            {
+                Text = "---select---",
+                Value = "0"
+            });
+
+            formatList.Insert(0, new SelectListItem() 
             {
                 Text = "---select---",
                 Value = "0"
@@ -61,6 +79,7 @@ namespace BookStore2024.Areas.Admin.Controllers
 
             ViewBag.Categories = cateList;
             ViewBag.Genres = genreList;
+            ViewBag.Formats = formatList;
             return View();
         }
         [HttpPost]
@@ -76,8 +95,10 @@ namespace BookStore2024.Areas.Admin.Controllers
                     string checkAuthor = formData.AuthorName ?? "";
 
                     var author = new TblAuthor();
-                    var existsAuthor = DBContext.TblAuthors.Any(p => p.AuthorName.Trim().ToLower() == checkAuthor.Trim().ToLower());
-                    if (!existsAuthor)
+                    author = DBContext.TblAuthors.Where(p => p.AuthorName.Trim().ToLower() == checkAuthor.Trim().ToLower()).FirstOrDefault();
+
+                    //var existsAuthor = DBContext.TblAuthors.Any(p => p.AuthorName.Trim().ToLower() == checkAuthor.Trim().ToLower());
+                    if (author == null)
                     {
                         author = new TblAuthor
                         {
@@ -86,10 +107,9 @@ namespace BookStore2024.Areas.Admin.Controllers
                         DBContext.TblAuthors.Add(author);
                         DBContext.SaveChanges();
                     }
-                    else
-                    {
-                        author = DBContext.TblAuthors.FirstOrDefault(p => p.AuthorName == checkAuthor);
-                    }
+                    
+                    author = DBContext.TblAuthors.FirstOrDefault(p => p.AuthorId == author.AuthorId);
+                    
                     //add book
 
                     var newBook = new TblBook
@@ -101,8 +121,8 @@ namespace BookStore2024.Areas.Admin.Controllers
                         Language = formData.Language,
                         IllustrationsNote = formData.IllustrationsNote,
                         Pages = formData.Pages,
-                        GenreId = formData.GenreId ?? 0,
-                        CategoryId = formData.CategoryId ?? 0
+                        GenreId = formData.GenreId,
+                        CategoryId = formData.CategoryId
                     };
 
                     if (ProductImg != null)
@@ -118,7 +138,7 @@ namespace BookStore2024.Areas.Admin.Controllers
                         BookId = newBook.BookId,
                         Isbn10 = formData.Isbn10,
                         Isbn13 = formData.Isbn13,
-                        FormatId = formData.FormatId ?? 0,
+                        FormatId = formData.FormatId,
                         StockQuantity = formData.StockQuantity ?? 0,
                         Price = formData.Price ?? 0,
                         Discount = formData.Discount,
@@ -128,18 +148,174 @@ namespace BookStore2024.Areas.Admin.Controllers
                 }
                 catch (Exception ex)
                 {
-                    TempData["Message"] = $"Eror" + ex;
+                    TempData["return"] = "Add - Input Eror";
+                    return View("Successed");
                 }
-                return View("AddSuccessed");
+                TempData["return"] = "Add Successed: " + formData.ProductName;
+                return View("Successed");
             }
-
-            return View(formData);
+            TempData["return"] = "Add - Database Eror";
+            return View("Successed");
         }
-        public IActionResult Edit()
+        #endregion
+
+        #region Edit
+        [HttpGet]
+        public IActionResult Edit(int detailID)
         {
-            return View();
+            var cateList = DBContext.TblCategories.Select(p => new SelectListItem()
+            {
+                Text = p.CategoryName,
+                Value = p.CategoryId.ToString()
+            }).ToList();
+
+            var genreList = DBContext.TblGenres.Select(p => new SelectListItem()
+            {
+                Text = p.GenreName,
+                Value = p.GenreId.ToString()
+            }).ToList();
+
+            var formatList = DBContext.TblFormats.Select(p => new SelectListItem()
+            {
+                Text = p.FormatName,
+                Value = p.FormatId.ToString()
+            }).ToList();
+
+            var productQuery = DBContext.ViewBookDetails.Where(p => p.BookDetailId == detailID).SingleOrDefault();
+
+            ProductVM product = new ProductVM()
+            {
+                BookDetailId = detailID,
+                ProductName = productQuery.BookTitle,
+                ProductDescription = productQuery.BookDescription,
+                GenreId = productQuery.GenreId,
+                GenreName = productQuery.GenreName,
+                CategoryId = productQuery.CategoryId,
+                CategoryName = productQuery.CategoryName,
+                Price = productQuery.Price,
+                Discount = productQuery.Discount,
+                Publisher = productQuery.Publisher,
+                ProductImg = productQuery.BookImageUrl,
+                AuthorId = productQuery.AuthorId,
+                AuthorName = productQuery.AuthorName,
+                Language = productQuery.Language,
+                Pages = productQuery.Pages,
+                IllustrationsNote = productQuery.IllustrationsNote,
+                Isbn10 = productQuery.Isbn10,
+                Isbn13 = productQuery.Isbn13,
+                FormatId = productQuery.FormatId,
+                FormatName = productQuery.FormatName,
+                StockQuantity = productQuery.StockQuantity,
+                BookId = productQuery.BookId,
+
+            };
+
+            cateList.Insert(0, new SelectListItem()
+            {
+                Text = product.CategoryName,
+                Value = product.CategoryId.ToString()
+            });
+
+            genreList.Insert(0, new SelectListItem()
+            {
+                Text = product.GenreName,
+                Value = product.GenreName.ToString()
+            });
+
+            formatList.Insert(0, new SelectListItem()
+            {
+                Text = product.FormatName,
+                Value = product.FormatId.ToString()
+            });
+            ViewBag.Categories = cateList;
+            ViewBag.Genres = genreList;
+            ViewBag.Formats = formatList;
+            ViewBag.Img = "../" + product.ProductImg;
+            return View(product);
         }
 
+        [HttpPost]
+        public IActionResult Edit(ProductVM formData, IFormFile? ProductImg, int? id)
+        {
+            if (ModelState.IsValid)
+            {
+                var product = DBContext.ViewBookDetails.SingleOrDefault(p => p.BookDetailId == id);
+                if (product != null)
+                {
+                    //author
+                    string checkAuthor = formData.AuthorName ?? "";
+
+                    var author = new TblAuthor();
+                    author = DBContext.TblAuthors.Where(p => p.AuthorName.Trim().ToLower() == checkAuthor.Trim().ToLower()).FirstOrDefault();
+
+                    //var existsAuthor = DBContext.TblAuthors.Any(p => p.AuthorName.Trim().ToLower() == checkAuthor.Trim().ToLower());
+                    if (author == null)
+                    {
+                        author = new TblAuthor
+                        {
+                            AuthorName = checkAuthor
+                        };
+                        DBContext.TblAuthors.Add(author);
+                        DBContext.SaveChanges();
+                    }
+
+                    author = DBContext.TblAuthors.FirstOrDefault(p => p.AuthorId == author.AuthorId);
+                    ////////////////////////////////////
+                    var book = DBContext.TblBooks.FirstOrDefault(p => p.BookId == product.BookId);
+
+                    if (book is not null)
+                    {
+                        if(formData.ProductName != null)
+                        {
+                            book.BookTitle = formData.ProductName;
+                        }
+                        if(author != null)
+                        {
+                            book.AuthorId = author.AuthorId;
+                        }
+                        book.BookDescription = formData.ProductDescription;
+                        book.Publisher = formData.Publisher;
+                        book.Language = formData.Language;
+                        book.IllustrationsNote = formData.IllustrationsNote;
+                        book.Pages = formData.Pages;
+                        book.GenreId = formData.GenreId;
+                        book.CategoryId = formData.CategoryId;
+                        if (ProductImg != null)
+                        {
+                            book.BookImageUrl = ProjectUtil.UploadImage(ProductImg, "books");
+                        }
+
+                        DBContext.Update(book);
+                        DBContext.SaveChanges();
+                    }
+                    ////////////////////////////////////
+                    var bookdetail = DBContext.TblBookDetails.FirstOrDefault(p => p.BookDetailId == product.BookDetailId);
+
+                    if (bookdetail is not null)
+                    {
+                        bookdetail.BookId = bookdetail.BookId;
+                        bookdetail.Isbn10 = formData.Isbn10;
+                        bookdetail.Isbn13 = formData.Isbn13;
+                        bookdetail.FormatId = formData.FormatId;
+                        bookdetail.StockQuantity = formData.StockQuantity ?? 0;
+                        bookdetail.Price = formData.Price ?? 0;
+                        bookdetail.Discount = formData.Discount ?? 0;
+
+                        DBContext.Update(bookdetail);
+                        DBContext.SaveChanges();
+                    }
+                    TempData["return"] = "Edited: " + formData.ProductName;
+                    return View("Successed");
+                }
+                
+               
+            }
+            TempData["return"] = "Fail to edited: " + formData.ProductName;
+            return View("Successed");
+        }
+        #endregion
+
+        #region Search
         public IActionResult Search(string? searchString)
         {
             var ProductsQuery = DBContext.ViewBookDetails.AsQueryable();
@@ -163,5 +339,6 @@ namespace BookStore2024.Areas.Admin.Controllers
             });
             return View("ProductList", data);
         }
+        #endregion
     }
 }
--- ViewModels/ProductVM.cs
+++ ViewModels/ProductVM.cs
@@ -2,18 +2,18 @@
 {
 	public class ProductVM
 	{
-        public int? BookDetailId { get; set; }
+        public int BookDetailId { get; set; }
         public string? ProductName { get; set; }
-        public int? GenreId { get; set; }
+        public int GenreId { get; set; }
         public string? GenreName { get; set; } = null!;
         public string? CategoryName { get; set; } = null!;
-        public int? CategoryId { get; set; }
+        public int CategoryId { get; set; }
         public decimal? Price { get; set; }
 		public decimal? Discount { get; set; } = 0;
         public string? Publisher { get; set; }
         public string? ProductImg { get; set; }
         public string? ProductDescription { get; set; }
-        public int? AuthorId { get; set; }
+        public int AuthorId { get; set; }
         public string? AuthorName { get; set; }
         public string? ProfileImageUrl { get; set; }
         public string? Language { get; set; }
@@ -21,10 +21,11 @@
         public string? IllustrationsNote { get; set; }
         public string? Isbn10 { get; set; }
         public string? Isbn13 { get; set; }
-        public int? FormatId { get; set; } = 1;
+        public int FormatId { get; set; } = 1;
         public string? FormatName { get; set; } = null!;
         public int? StockQuantity { get; set; }
         public int? Views { get; set; }
+        public int BookId { get; set; }
 
     }
 }
